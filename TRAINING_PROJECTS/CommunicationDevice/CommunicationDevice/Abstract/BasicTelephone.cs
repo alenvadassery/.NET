2229f952bf6a0adac4ff51c2f8a70c3a02c3fb88
Alen Vadassery <alenvadassery@outlook.com>
2020-01-23 02:26:23 +0530

    Updated Repository structure.

---
+++ TRAINING_PROJECTS/CommunicationDevice/CommunicationDevice/Abstract/BasicTelephone.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneProject.Abstract
+{
+	abstract class BasicTelephone
+	{
+		public int PhoneNo { get; set; }
+		public bool ConnectionStatus { get; set; }
+		public bool PowerStatus { get; set; }
+
+		public virtual bool SwitchOn()
+		{
+			Console.WriteLine("Switching on feature from Basic Telephone");
+			PowerStatus = true;
+
+			return PowerStatus;
+		}
+		public virtual bool SwitchOff()
+		{
+			Console.WriteLine("Switching off feature from Basic Telephone");
+			PowerStatus = false;
+
+			return PowerStatus;
+		}
+
+		public abstract string Dial();
+	}
+}
---
+++ TRAINING_PROJECTS/CommunicationDevice/CommunicationDevice/Interface/IBaiscTelephone.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneProject.Interface
+{
+	public interface IBaiscTelephone
+	{
+		 int PhoneNo { get; set; }
+		 bool ConnectionStatus { get; set; }
+		bool PowerStatus { get; set; }
+
+		bool SwitchOn();
+		bool SwitchOff();
+		string Dial();
+	}
+}
