3453db5ea2119eb5d2a5eb54b7fb14347eb3d1ad
Alen Vadassery <alenvadassery@outlook.com>
2020-01-23 03:19:33 +0530

    Added basic abstract classes.

---
+++ TRAINING_PROJECTS/CommunicationDevice/CommunicationDevice/Abstract/CommunicationDevice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneProject.Abstract
+{
+	abstract class CommunicationDevice
+	{
+		public bool PowerStatus { get; set; }
+		public abstract bool SwitchOn();
+
+		public abstract bool SwitchOff();
+
+		public virtual void SelfDiagnose()
+		{
+			Console.WriteLine("Running self diagnosis using basic CommunicationDevice kernel");
+		}
+	}
+}
---
+++ TRAINING_PROJECTS/CommunicationDevice/CommunicationDevice/Abstract/Telephone.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneProject.Abstract
+{
+	abstract class Telephone : CommunicationDevice
+	{
+		public int PhoneNo { get; set; }
+		public bool ConnectionStatus { get; set; }
+		//public bool PowerStatus { get; set; }
+		public bool IsDialing { get; set; }
+
+		public override bool SwitchOn()
+		{
+			Console.WriteLine("Switching on feature from Basic Telephone using Power Button");
+			
+			return base.PowerStatus = true;
+		}
+		public override bool SwitchOff()
+		{
+			Console.WriteLine("Switching off feature from Basic Telephone using Power Button");
+			
+			return base.PowerStatus = false;
+		}
+
+		public virtual bool Dial(int phoneNo)
+		{
+			if (!IsDialing)
+			{
+				Console.WriteLine("Dilaing " + phoneNo + " from Basic Telephone");
+
+				IsDialing = true;
+			}
+
+			return IsDialing;
+		}
+	}
+}
